e4cb0902c17df0d5023d59cda3f9db97be3a3da4
Anjali7120 <anjalikapoor7120@gmail.com>
2024-01-15 21:24:12 +0530

    new changes for pre-commit script

--- scripts/pre-commit-script.sh
+++ scripts/pre-commit-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# BRANCH=`git rev-parse --abbrev-ref HEAD`; if [ \"$BRANCH\" = \"main\" ]; then echo \"No direct commit to main. Please push changes to a new branch and create a PR to main.\"; git reset HEAD; exit 1; fi;
+BRANCH=`git rev-parse --abbrev-ref HEAD`; if [ \"$BRANCH\" = \"main\" ]; then echo \"No direct commit to main. Please push changes to a new branch and create a PR to main.\"; git reset HEAD; exit 1; fi;
 
 echo 'Pre-commit checks like linting'  
 
